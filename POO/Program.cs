c86a0b0e19e50caf0e45858bbb1011e930cd4251
Nuno Rocha <nuno.teles.rocha@gmail.com>
2026-02-04 23:33:13 +0000

    Novas aulas adicionadas

---
+++ POO/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objeto é sempre um Tipode Refeência : só armazena o endereço dos dados
+
+            var pagamentoCR = new PagamentoPrestacoes();
+            pagamentoCR.Pagar();
+            pagamentoCR.Vencimento = DateTime.Now;
+            pagamentoCR.NumeroPrestacao = "123456789";
+            
+            var pagamento = new Pagamento();
+            pagamento.Vencimento = DateTime.Now;
+
+            Console.WriteLine(pagamento.DataPagamento);
+        }
+
+        public class Pagamento
+        {
+            // Propriedades - (Variáveis)
+
+            public DateTime Vencimento { get; set; }
+            public DateTime _dataPagaemnto { get; private set; }
+
+            private DateTime _dataPagamento;
+
+            public DateTime DataPagamento
+            {
+                get 
+                {
+                    Console.WriteLine("A imprimir o valor");
+                    return _dataPagamento; 
+                }
+                set 
+                {
+                    Console.WriteLine("A alterar o valor");
+                    _dataPagamento = value;
+                } 
+            }
+
+            public Morada MoradaEnvio;
+
+            // Eventos - (Funções) 
+
+            public virtual void Pagar()
+            {
+
+            }
+                
+            private void Receber() { }
+
+            protected void Saldo() { }
+        }
+
+        public class PagamentoPrestacoes : Pagamento
+        {
+            public string NumeroPrestacao;
+
+            public override void Pagar()
+            {
+                // Regra só pagamento a prestações
+            }
+        }
+
+        public class PagamentoCartaoDeCredito : Pagamento
+        {
+            public string NumeroCartao;
+
+            public override void Pagar()
+            {
+                // Regra só pagamento com o cartão de crédito
+            }
+        }
+
+        public class Morada
+        {
+            public string CodigoPostal;
+        }
+    }
+}
---
+++ POO_Pratica/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace POO_Pratica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var esc = new Escuteiro();
+            esc.Nome = "Nuno";
+            esc.ImprimeNome();
+            esc.Cumprimenta();
+            Console.WriteLine(esc.Nome);
+        }
+
+        class Pessoa
+        {
+            public string Nome;
+            public int Idade;
+            public string Morada;
+
+            public void ImprimeNome()
+            {
+                Console.WriteLine(Nome);
+            }
+
+            public virtual void Cumprimenta()
+            {
+
+            }
+        }
+
+        class Escuteiro : Pessoa
+        {
+            string Seccao;
+            string NomeEquipa;
+
+
+            public override void Cumprimenta()
+            {
+                Console.WriteLine($"Olá {Nome}!");
+            }
+        }
+    }
+}

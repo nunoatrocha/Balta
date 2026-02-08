36c8c0b74142b791ad834a5c16d5d8b12559ce89
Nuno Rocha <nuno.teles.rocha@gmail.com>
2026-02-08 19:22:45 +0000

    Novas aulas adicionadas

--- POO/obj/Release/net10.0/POO.AssemblyInfo.cs
+++ POO/obj/Release/net10.0/POO.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77bc6d765223f79016805d4ab4a581ff067ce7cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
--- POO_Pratica/Program.cs
+++ POO_Pratica/Program.cs
@@ -4,33 +4,46 @@
     {
         static void Main(string[] args)
         {
-            var esc = new Escuteiro("Nuno", 46, "Rua de Cima");
-            esc.ImprimeNome();
-            esc.Cumprimenta();
-            Console.WriteLine(esc.Nome);
+            Escuteiro e1 = new Escuteiro ("Rita", new DateTime(2010, 8, 10), "Rua de Ciema", "Pioneiros", "Pantera");
+            e1.Cumprimenta();
         }
 
         public class Pessoa
         {
+
+            // PARAMETROS ----------------------
+
             public string Nome { get; set; }
-            public int Idade { get; set; }
+            public DateTime DataNascimento { get; set; }
             public string Morada { get; set; }
 
-            public Pessoa(string nome, int idade, string morada )
+            public int Idade
+            {
+                get
+                {
+                    int idade = DateTime.Now.Year - DataNascimento.Year;
+
+                    if (DateTime.Now.DayOfYear < DataNascimento.DayOfYear)
+                        idade--;
+
+                    return idade;
+                }
+            }
+
+            // METODO CONSTRUTOR ---------------
+
+            public Pessoa(string nome, DateTime datanascimento, string morada)
             {
                 Nome = nome;
-                Idade = idade;
+                DataNascimento = datanascimento;
                 Morada = morada;
             }
 
-            public void ImprimeNome()
-            {
-                Console.WriteLine(Nome);
-            }
+            //  METODO DA CLASSE PESSOA
 
             public virtual void Cumprimenta()
             {
-                Console.WriteLine($"Olá {Nome} => Metodo Pai");
+                Console.WriteLine($"Olá eu sou o/a {Nome} e tenho {Idade} anos!");
             }
         }
 
@@ -39,16 +52,37 @@
             public string Seccao { get; set; }
             public string NomeEquipa { get; set; }
 
-            public Escuteiro(string nome, int idade, string morada) 
-                : base(nome, idade, morada)
+            public Escuteiro(string nome, DateTime datanascimento, string morada, string seccao, string nomeEquipa)
+                :base(nome, datanascimento, morada )
             {
+                Seccao = seccao;
+                NomeEquipa = nomeEquipa;
             }
 
             public override void Cumprimenta()
             {
-                base.Cumprimenta();
-                Console.WriteLine($"Olá {Nome}!");
+                Console.WriteLine($"Olá o meu nome é {Nome}, tenho {Idade} e estou nos {Seccao} na equipa {NomeEquipa}");
             }
+
         }
+
+        public class Dirigente : Escuteiro
+        {
+            public string Cargo { get; set; }
+
+            public Dirigente(string nome, DateTime datanascimento, string morada, string seccao, string nomeEquipa, string cargo)
+                :base(nome, datanascimento, morada, seccao, nomeEquipa)
+            {
+                Cargo = cargo;
+            }
+
+            public override void Cumprimenta()
+            {
+                Console.WriteLine($"Olá eu {Cargo} nos {Seccao}");
+            }
+
+
+        }
+
     }
 }
